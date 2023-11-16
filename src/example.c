3b3abad87559f31e8a9f3ce30c4005d718acb6c5
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2023-11-16 21:04:03 +0100

    Test framework

---
+++ src/example.c
@@ -0,0 +1,4 @@
+#include "example.h"
+#include <stdio.h>
+
+void this_is_an_example() { printf("Yay"); }
---
+++ src/example.h
@@ -0,0 +1,8 @@
+// Example on how to link a header file to a test file
+// It should be removed in the future
+#ifndef EXAMPLE_H
+#define EXAMPLE_H
+
+void this_is_an_example();
+
+#endif // EXAMPLE_H
