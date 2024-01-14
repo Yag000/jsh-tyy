dfd93623e6bdd2d647b809d600712a734dd89f99
IGLESIAS VAZQUEZ yago <yago.iglesias-vazquez@etu.u-paris.fr>
2024-01-14 13:19:14 +0100

    refactor(jobs): remove job type
    This is unnecessary and complicates the code. It was never actually used
    just set to whatever the states was, but no need to have it.

--- tests/utils.h
+++ tests/utils.h
@@ -18,11 +18,11 @@ void helper_mute_update_jobs(char *file_name);
 
 command_result *mute_command_execution(command *);
 
-job *new_single_command_job(command_call *, pid_t, job_status, job_type);
+job *new_single_command_job(command_call *, pid_t, job_status);
 
 /* Returns a new job with the given command, pid, status and type.
  * All the subjobs will have the same status, type and pid.
  */
-job *job_from_command(command *command, pid_t pid, job_status status, job_type type);
+job *job_from_command(command *command, pid_t pid, job_status status);
 
 #endif // UTILS_H
